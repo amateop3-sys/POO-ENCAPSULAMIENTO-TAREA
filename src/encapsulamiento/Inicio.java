5594ca6c24dc707dd8ecfc1355433ed6233be106
amateop3-sys <amateop3@miumg.edu.gt>
2026-03-24 16:30:02 -0600

    EJERCICIO 1 - CLASE PERSONA

--- src/encapsulamiento/Inicio.java
+++ src/encapsulamiento/Inicio.java
@@ -3,8 +3,14 @@ package encapsulamiento;
 public class Inicio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Persona pers1= new Persona("PEDRO JOSÉ", "PÉREZ ALAY", 58);
+		Persona pers2= new Persona("IVAN","SALAZAR",22);
+		Persona pers3= new Persona("MARÍA BEATRIZ", "HERNÁNDEZ",31);
+		
+		pers1.mostrarPersona();
+		pers2.mostrarPersona();
+		pers3.mostrarPersona();
+	
 	}
 
 }
--- src/encapsulamiento/Persona.java
+++ src/encapsulamiento/Persona.java
@@ -2,4 +2,40 @@ package encapsulamiento;
 
 public class Persona {
 
+	// atributos
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	// constructor
+	public Persona(String nombre, String apellido, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+	}
+	
+	
+
+	
+	public void mostrarPersona() {
+		
+		System.out.println("Su nombre es: "+nombre);
+		System.out.println("Su apellido es: "+apellido);
+		System.out.println("Su edad es: "+edad);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
