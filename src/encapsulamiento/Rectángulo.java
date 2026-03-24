734f617f1751a5df81fbb50d7c47d86901dea26c
amateop3-sys <amateop3@miumg.edu.gt>
2026-03-24 16:53:09 -0600

    EJERCICIO 2 - CLASE RECTÁNGULO

--- src/encapsulamiento/Rectángulo.java
+++ src/encapsulamiento/Rectángulo.java
@@ -1,5 +1,31 @@
 package encapsulamiento;
 
 public class Rectángulo {
+	
+	private float base;
+	private float altura;
+	
+
+	
+	
+	
+	public Rectángulo(float base, float altura) {
+		super();
+		this.base = base;
+		this.altura = altura;
+	}
+
+
+
+
+
+	public double area() {
+		return base*altura;
+	}
+	
+	public double perimetro() {
+		return 2*(base+altura);
+		
+	}
 
 }
