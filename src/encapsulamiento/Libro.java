4788f66233756ad066745e03a4508ba2e7559d0b
amateop3-sys <amateop3@miumg.edu.gt>
2026-03-25 02:50:48 -0600

    EJERCICIO 5 - CLASE LIBRO

--- src/encapsulamiento/Libro.java
+++ src/encapsulamiento/Libro.java
@@ -2,4 +2,30 @@ package encapsulamiento;
 
 public class Libro {
 
+	private String titulo;
+	private String autor;
+	private int páginas;
+	
+	
+	public Libro(String titulo, String autor, int páginas) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.páginas = páginas;
+	}
+	
+	
+	
+	public boolean esLargo() {
+		return páginas >=300;
+	}
+	
+	public void mostrarLibro() {
+		
+		System.out.println("EL TITULO ES: "+titulo);
+		System.out.println("EL AUTOR ES: "+autor);
+		System.out.println("CANTIDAD DE PÁGINAS: "+páginas);
+		
+	}
+	
 }
