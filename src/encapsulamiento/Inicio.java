8a097b58eb625ba7482715085884931e3d4834b5
amateop3-sys <amateop3@miumg.edu.gt>
2026-03-24 20:54:45 -0600

    EJERCICIO 3 - CLASE PRODUCTO

--- src/encapsulamiento/Inicio.java
+++ src/encapsulamiento/Inicio.java
@@ -3,6 +3,8 @@ package encapsulamiento;
 public class Inicio {
 
 	public static void main(String[] args) {
+		
+		// EJERCICIO 1
 		Persona pers1= new Persona("PEDRO JOSÉ", "PÉREZ ALAY", 58);
 		Persona pers2= new Persona("IVAN","SALAZAR",22);
 		Persona pers3= new Persona("MARÍA BEATRIZ", "HERNÁNDEZ",31);
@@ -11,6 +13,35 @@ public class Inicio {
 		pers2.mostrarPersona();
 		pers3.mostrarPersona();
 	
-	}
+		
+	System.out.println("--------------------------");
+	
 
+	// EJERCICIO 2
+	Rectángulo rec1= new Rectángulo(25,4);
+	Rectángulo rec2= new Rectángulo(75,6);
+	
+	if(rec1.area()>rec2.area()) {
+		System.out.println("EL rectangulo 1 tiene mayor area");
+	}
+	else {
+		System.out.println("EL rectangulo 2 tiene mayor area");
+	}
+	
+	
+	
+	System.out.println("--------------------------");
+	
+	
+	Producto prod1 = new Producto("01", "Computadora acer", 9949.69);
+	Producto prod2 = new Producto("02","Lays", 4.00);
+	
+	prod1.aplicarDescuento(50);	
+	prod2.aplicarDescuento(4);
+	
+	prod1.mostrarProducto();
+	prod2.mostrarProducto();
+	
+}
+	
 }
